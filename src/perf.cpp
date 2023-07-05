f1e81a6cac15c28a0e1f917406b2ee9cc486d4c0
AaronTheNerd <bargeaaron@gmail.com>
2023-07-04 21:16:49 -0400

    Chore: modified perf.cpp to accept command line arguments to avoid recompilation when wanting to test in different conditions

--- src/perf.cpp
+++ src/perf.cpp
@@ -1,21 +1,88 @@
 #include <chrono>
+#include <cstring>
 #include <iostream>
+#include <string>
+
+#include "calculator.h"
 #include "generation.h"
+
+CALC_ELEM cmd_char_to_elem(char c) {
+  switch (c) {
+    case 'a':
+      return CALC_PI;
+    case 'b':
+      return CALC_1;
+    case 'c':
+      return CALC_2;
+    case 'd':
+      return CALC_3;
+    case 'e':
+      return CALC_4;
+    case 'f':
+      return CALC_5;
+    case 'g':
+      return CALC_6;
+    case 'h':
+      return CALC_7;
+    case 'i':
+      return CALC_8;
+    case 'j':
+      return CALC_9;
+    case 'k':
+      return CALC_10;
+    case 'l':
+      return CALC_HALF;
+    case 'm':
+      return CALC_THIRD;
+    case 'n':
+      return CALC_QUARTER;
+    case 'o':
+      return CALC_A;
+    case 'p':
+      return CALC_B;
+    case 'q':
+      return CALC_C;
+    case 'r':
+      return CALC_E;
+    case 's':
+      return CALC_H;
+    case 't':
+      return CALC_SQRT;
+    case 'u':
+      return CALC_NEG;
+    case 'v':
+      return CALC_INV;
+    case 'w':
+      return CALC_ADD;
+    case 'x':
+      return CALC_SUB;
+    case 'y':
+      return CALC_MUL;
+    case 'z':
+      return CALC_DIV;
+    case '0':
+      return CALC_EXP;
+  }
+  return CALC_INVALID;
+}
+
+CALC cmd_string_to_calc(char* s) {
+  CALC result;
+  for (uint8_t i = 0; i < strlen(s); ++i) {
+    result += cmd_char_to_elem(s[i]);
+  }
+  return result;
+}
 
 int main(int argc, char** argv) {
   auto start = std::chrono::high_resolution_clock::now();
-  CALC initial_calc =
-      std::string() + CALC_PI + CALC_A + CALC_B + CALC_ADD + CALC_MUL;
-  atn::Calculator c{initial_calc, 6};
-  CALC all_elems = std::string() + CALC_ADD + CALC_SUB + CALC_MUL + CALC_DIV +
-                   CALC_EXP + CALC_SQRT + CALC_NEG + CALC_INV + CALC_PI +
-                   CALC_A + CALC_B + CALC_C + CALC_E + CALC_H + CALC_1 +
-                   CALC_2 + CALC_3 + CALC_4 + CALC_5 + CALC_6 + CALC_7 +
-                   CALC_8 + CALC_9 + CALC_10 + CALC_HALF + CALC_THIRD +
-                   CALC_QUARTER;
+  CALC initial_calc = cmd_string_to_calc(argv[1]);
+  atn::Calculator c{initial_calc, std::stoi(argv[2])};
+  CALC all_elems = cmd_string_to_calc(argv[3]);
   atn::Generator gen(0, 1, c, all_elems, true);
   gen.run();
   auto end = std::chrono::high_resolution_clock::now();
-  auto runtime = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+  auto runtime =
+      std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
   std::cout << runtime.count() << std::endl;
 }
